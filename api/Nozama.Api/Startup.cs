46d3ca7f8417646bfc325f69c2e02e0ba1ca85e3
Jorge Mederos Alvarado <jmederosalvarado@gmail.com>
2019-11-22 22:43:30 -0500

    Setup Api Startup

--- api/Nozama.Api/Startup.cs
+++ api/Nozama.Api/Startup.cs
@@ -10,6 +10,7 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
+using Nozama.Persistence.Contexts;
 
 namespace Nozama.Api
 {
@@ -25,20 +26,20 @@ namespace Nozama.Api
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+
             services.AddControllers();
+            // services.AddDbContext<NozamaContext>()
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
-            if (env.IsDevelopment())
-            {
-                app.UseDeveloperExceptionPage();
-            }
-
-            app.UseHttpsRedirection();
-
             app.UseRouting();
+
+            app.UseCors(b => b
+                .AllowAnyOrigin()
+                .AllowAnyMethod()
+                .AllowAnyHeader());
 
             app.UseAuthorization();
 
